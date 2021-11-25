f2ef99ae90ce8c18ab826e9e5d5a83f8cc31b367
Autumn_Saury <autumnsaury@outlook.com>
2021-11-25 21:38:42 +0800

    Color Modifications.

--- Akina/post.php
+++ Akina/post.php
@@ -116,8 +116,7 @@
 		</article>
 		<!-- 版权声明 -->
 		<div class="open-message">
-			<p>声明：<?php $this->options->title() ?>|版权所有，违者必究|如未注明，均为原创|本网站采用<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">BY-NC-SA</a>协议进行授权</p>
-			<p>转载：转载请注明原文链接 - <a href="<?php $this->permalink(); ?>"><?php $this->title() ?></a></p>	
+			<p>本站所有文章均采用<a href="https://creativecommons.org/licenses/by-nc-sa/3.0/" target="_blank">CC BY-NC-SA 3.0</a>协议进行授权</p>	
 		</div>
 		<!-- 相邻文章 -->
 		<section class="post-squares nextprev">
